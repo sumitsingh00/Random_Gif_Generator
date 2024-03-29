7f8a13371628ef5c9ff0f8d54598c455a307136f
sumitsingh00 <sumit00134@gmail.com>
2024-03-30 00:35:38 +0530

    Axios

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,5 +1,5 @@
 // import React from 'react'
-import axios from 'axios';
+import axios from 'axios';// its same as fetch data nothing difference
 import { useEffect } from 'react';
 import { useState } from 'react';
 
